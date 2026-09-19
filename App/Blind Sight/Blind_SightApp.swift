b93c1e134f853f66eced041e202fc2e8b8b93880
FlorenceYann <163768754+FlorenceYann@users.noreply.github.com>
2026-09-19 16:45:46 -0400

    App Added

---
+++ App/Blind Sight/Blind_SightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blind_SightApp.swift
+//  Blind Sight
+//
+//  Created by Flora Yan on 2026-09-19.
+//
+
+import SwiftUI
+
+@main
+struct Blind_SightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
